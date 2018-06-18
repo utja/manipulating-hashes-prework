5b213b1c22280cf950b5639da4f382e92771f348
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-18 02:52:15 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,8 +15,16 @@ def first_challenge
 
   #your code here
 
-
-  #remember to return your newly altered contacts hash!
-  contacts
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+        end
+      end
+    end
+  end
+    #remember to return your newly altered contacts hash!
+    contacts
 end
 
